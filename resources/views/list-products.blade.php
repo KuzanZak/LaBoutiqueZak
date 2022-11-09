5f99fb0ff8c3d664b34c5cf2db2e3f40e60f8b75
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-11-09 15:30:51 +0100

    listproducts page: ongoing on mobile

--- resources/views/list-products.blade.php
+++ resources/views/list-products.blade.php
@@ -6,11 +6,18 @@
         @foreach($products as $product)
         <li class="list-products-items">
             <img class="main-image" src="{{$product->mainImage->url}}" alt="{{$product->mainImage->alt}}">
-            <div>
+            <div class="page-product-description">
                 <h2 class="title-product">{{$product->product_name}}</h2>
                 <div class="rating-product">Note : ??/5</div>
                 <div class="price-product">{{$product->price}}€</div>
-                <div>Disponibilité : {{$product->stock > 0 ? 'En stock' : 'Indisponible'}}</div>
+                <div>{{$product->stock > 0 ? 'En stock' : 'Indisponible'}}</div>
+                <div class="add-buttons">
+                    <a href="#">
+                        <i class="fa fa-star add-to-favorite" aria-hidden="true"></i>
+                        <!-- <i class="fa fa-star-o" aria-hidden="true"></i> -->
+                    </a>
+                    <a href="#"><i class="fa fa-shopping-basket add-to-shopping-cart" aria-hidden="true"></i></a>
+                </div>
             </div>
         </li>
         @endforeach
